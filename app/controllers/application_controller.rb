92ab5e7e749095fa75f1665d2108f72481540018
kensei1016 <kensei1016ab@gmail.com>
2020-04-29 14:47:50 +0900

    ユーザのサインアップにnameを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_search_suread
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   private
   def set_search_suread
